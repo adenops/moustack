ed4118b529de1cf804e3727db59843d5c3ea355a
Lionel Nicolas <lionel.nicolas@adenops.com>
2016-04-05 15:39:20 -0400

    docker/ubuntu-base: refactor image build

--- docker/ubuntu-base/cleanup_tmp_folders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Note: starting with Docker 1.10 there will be a --tmpfs option that should be
-# a better solution than manually removing everything.
-
-echo "cleaning up /run and /tmp"
-find /run/ ! -path '*/netns/*' -type f -delete 2>/dev/null || true
-rm -rf /tmp/* 2>/dev/null || true
