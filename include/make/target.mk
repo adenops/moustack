5a8af2c4b0bc118458c168ae7e17661bf64d89da
Lionel Nicolas <lionel.nicolas@adenops.com>
2016-11-29 22:15:01 -0500

    make: remove 'latest' tag support

--- include/make/target.mk
+++ include/make/target.mk
@@ -20,19 +20,18 @@ distclean: clean
 	@$(call check_defined,DOCKER_TAG)
 
 	@$(call logme,"Untag release image $(DOCKER_IMAGE)")
-	docker rmi -f $(DOCKER_REGISTRY)/$(DOCKER_IMAGE) || true
 	docker rmi -f $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):$(DOCKER_TAG) || true
 
 	@$(call logme,"Remove image $(DOCKER_IMAGE)")
-	docker rmi -f $(DOCKER_IMAGE) || true
 	docker rmi -f $(DOCKER_IMAGE):$(DOCKER_TAG) || true
 
 
 $(TRIGGER_BUILD):
 	@$(call check_defined,DOCKER_IMAGE)
+	@$(call check_defined,DOCKER_TAG)
 
 	@$(call logme,"Build image $(DOCKER_IMAGE)")
-	docker build --file Dockerfile --tag $(DOCKER_IMAGE) $(DOCKER_BUILD_ARGS) --no-cache=$(DOCKER_NO_CACHE) --force-rm .
+	docker build --file Dockerfile --tag $(DOCKER_IMAGE):$(DOCKER_TAG) $(DOCKER_BUILD_ARGS) --no-cache=$(DOCKER_NO_CACHE) --force-rm .
 
 	@mkdir -p $(TRIGGER_DIR)
 	@touch $(TRIGGER_BUILD)
@@ -42,15 +41,14 @@ build: $(TRIGGER_BUILD)
 
 
 $(TRIGGER_RELEASE):
+	@$(call check_defined,DOCKER_IMAGE)
 	@$(call check_defined,DOCKER_TAG)
 
-	@$(call logme,"Tag image $(DOCKER_IMAGE)")
-	docker tag $(DOCKER_IMAGE):latest $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):$(DOCKER_TAG)
-	docker tag $(DOCKER_IMAGE):latest $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):latest
+	@$(call logme,"Tag image $(DOCKER_IMAGE):$(DOCKER_TAG)")
+	docker tag $(DOCKER_IMAGE):$(DOCKER_TAG) $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):$(DOCKER_TAG)
 
-	@$(call logme,"Push image $(DOCKER_IMAGE)")
+	@$(call logme,"Push image $(DOCKER_IMAGE):$(DOCKER_TAG)")
 	docker push $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):$(DOCKER_TAG)
-	docker push $(DOCKER_REGISTRY)/$(DOCKER_IMAGE):latest
 
 	@mkdir -p $(TRIGGER_DIR)
 	@touch $(TRIGGER_RELEASE)
